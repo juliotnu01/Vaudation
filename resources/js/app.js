ce9c6a603aae4eec8058f4daaf327c45f1d81b3c
juliot <nunezjuliot@gmail.com>
2020-04-07 11:20:57 -0500

    feat: making: autition

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import vuetify from './src/plugins/vuetify'
+import router from './src/plugins/router'
+import Vuex from 'vuex'
+import { store } from './src/plugins/store'
+
+Vue.use(Vuex)
+window.Vue = require('vue');
+
+
+const app = new Vue({
+    vuetify,
+    router,
+    store
+}).$mount('#app');
